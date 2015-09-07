2de4b187a623ce782ed1bd5899b7208aaee170e2
jxing2 <jinghaoxing1990@gmail.com>
2015-09-07 08:38:10 -0400

    Update test.h

--- src/test.h
+++ src/test.h
@@ -1,1 +1,27 @@
 // test.h
+#ifndef INCLUDED_TEST
+#define INCLUDED_TEST
+
+#include <iostream>
+
+namespace StevensDev
+{
+  class Test
+  {
+    private:
+      int test;
+        // For testing.
+    public:
+      // CONSTRUCTORS
+      Test( int testNumber );
+        // Constructor utilizing test number.
+  };
+  
+  // CONSTRUCTORS
+  inline
+  Test::Test( int testNumber ) : test(testNumber)
+  {
+    std::cout << "The test number is " << test;
+  }
+  
+}
